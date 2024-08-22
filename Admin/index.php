7d32b88a8855094a5e48c6788440c96bc73d71ac
DarkNess1903 <nes0953575427@gmail.com>
2024-08-22 18:18:50 +0700

    Update index.php
    หลังบ้าน

--- Admin/index.php
+++ Admin/index.php
@@ -338,8 +338,8 @@ if (!isset($_SESSION['admin_id'])) {
                         });
                     </script>
 
-                        <!-- Area Chart -->
-                        <div class="col-xl-8 col-lg-7">
+                         <!-- Area Chart -->
+                         <div class="col-xl-8 col-lg-7">
                             <div class="card shadow mb-4">
                                 <!-- Card Header - Dropdown -->
                                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
